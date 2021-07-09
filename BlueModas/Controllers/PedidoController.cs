5e9b068d55fc0100f45684742bf9e5c407247eb7
Lucas Martins <lucasmcosta0104@gmail.com>
2021-07-09 03:08:56 -0300

    Blue Modas

---
+++ BlueModas/Controllers/PedidoController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlueModas.Controllers
+{
+    public class PedidoController : Controller
+    {
+        public ActionResult Carrossel()
+        {
+            return View();
+        }
+
+        public ActionResult Carrinho(int id)
+        {
+            return View();
+        }
+
+        public ActionResult Cadastro()
+        {
+            return View();
+        }
+
+        public ActionResult Resumo()
+        {
+            return View();
+        }
+
+
+    }
+}
